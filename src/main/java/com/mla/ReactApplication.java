a8c2c00c00413e485e3d1ba8b61f9eaaa605038a
Rajesh Nallamothu <raja2rajesh@gmail.com>
2018-04-18 12:24:43 +0530

    intial commit after gc push

--- src/main/java/com/mla/ReactApplication.java
+++ src/main/java/com/mla/ReactApplication.java
@@ -1,11 +1,18 @@
 package com.mla;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ReactApplication {
+public class ReactApplication extends SpringBootServletInitializer
+{
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder ReactApplication) {
+		return ReactApplication.sources(ReactApplication.class);
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(ReactApplication.class, args);
 	}
--- src/main/java/com/mla/controllers/TopicController.java
+++ src/main/java/com/mla/controllers/TopicController.java
@@ -17,19 +17,19 @@ import java.util.Optional;
  * Created by rnallamothu on 4/15/18.
  */
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "http://35.229.43.8:8080")
 public class TopicController {
     @Autowired
     TopicRepository topicRepository;
 
     @RequestMapping(method= RequestMethod.GET, value="/topics")
-    @CrossOrigin(origins = "http://localhost:4200")
+    @CrossOrigin(origins = "http://35.229.43.8:8080")
     public Iterable<Topic> topic() {
         return topicRepository.findAll();
     }
 
     @RequestMapping(method=RequestMethod.POST, value="/topics")
-    @CrossOrigin(origins = "http://localhost:4200")
+    @CrossOrigin(origins = "http://35.229.43.8:8080")
     public Topic save(@RequestBody Topic topic) {
         System.out.println("--------"+topic.getTopicName());
         topicRepository.save(topic);
@@ -37,13 +37,13 @@ public class TopicController {
     }
 
     @RequestMapping(method=RequestMethod.GET, value="/topics/{id}")
-    @CrossOrigin(origins = "http://localhost:4200")
+    @CrossOrigin(origins = "http://35.229.43.8:8080")
     public Optional<Topic> show(@PathVariable String id) {
         return topicRepository.findById(id);
     }
 
     @RequestMapping(method=RequestMethod.PUT, value="/topics/{id}")
-    @CrossOrigin(origins = "http://localhost:4200")
+    @CrossOrigin(origins = "http://35.229.43.8:8080")
     public Topic update(@PathVariable String id, @RequestBody Topic topic) {
         Optional<Topic> optcontact = topicRepository.findById(id);
         Topic t = optcontact.get();
